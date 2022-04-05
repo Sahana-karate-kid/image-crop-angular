6357ea52e47cfc56a723ad6155987f94fb0691de
Sahana-karate-kid <sahanakorlahalli@gmail.com>
2022-04-05 21:15:52 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { ImageCroppedEvent } from 'ngx-image-cropper';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'image-crop-angular';
+  imgChangeEvt: any = '';
+  cropImgPreview: any = '';
+  uploadImage: any='';
+  roundCropper: any='true';
+
+  onFileChange(event: any): void {
+    this.imgChangeEvt = event;
+  }
+  cropImg(e: ImageCroppedEvent) {
+    this.cropImgPreview = e.base64;
+    this.uploadImage= e.base64;
+    console.log(this.uploadImage);
+  }
+
+  imgLoad() {
+    console.log('loaded');
+  }
+
+  initCropper() {
+    // init cropper
+  }
+
+  imgFailed() {
+    console.log('Loading failed');
+  }
+ 
+ 
 }
